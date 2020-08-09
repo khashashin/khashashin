7cdf75fe9b51029c539a958c319a18ef8492a1db
Yusuf Khasbulatov <khashashin@gmail.com>
2020-08-09 22:15:38 +0200

    moved back-end code under server folder

--- server/khashashin/manage.py
+++ server/khashashin/manage.py
@@ -5,7 +5,10 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'khashashin.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'khashashin.settings.dev')
+
+    print('Starting manage.py using settings: ', os.environ.get('DJANGO_SETTINGS_MODULE'))
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -13,7 +16,7 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        ) from exc
+        )(exc)
     execute_from_command_line(sys.argv)
 
 
